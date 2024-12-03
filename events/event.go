d0b67cc6088c3a24495acb47ca1d9fdc965bcc48
Luiggy102 <79861715+Luiggy102@users.noreply.github.com>
2024-12-03 16:40:36 -0500

    added abstract implementation for Message broker

---
+++ events/event.go
@@ -0,0 +1,31 @@
+package events
+
+import (
+	"context"
+
+	"github.com/Luiggy102/go-cqrs-eda/models"
+)
+
+type EventStore interface {
+	Close()
+	PublishCreatedFeed(ctx context.Context, feed *models.Feed) error
+	SubscribeCreatedFeed(ctx context.Context) (<-chan CreatedFeedMessage, error)
+	OnCreatedFeed(ctx context.Context, f func(CreatedFeedMessage)) error
+}
+
+var eventStore EventStore
+
+// set the abstract implementation
+func SetEventStrore(store EventStore) {
+	eventStore = store
+}
+
+func PublishCreatedFeed(ctx context.Context, feed *models.Feed) error {
+	return eventStore.PublishCreatedFeed(ctx, feed)
+}
+func SubscribeCreatedFeed(ctx context.Context) (<-chan CreatedFeedMessage, error) {
+	return eventStore.SubscribeCreatedFeed(ctx)
+}
+func OnCreatedFeed(ctx context.Context, f func(CreatedFeedMessage)) error {
+	return eventStore.OnCreatedFeed(ctx, f)
+}
