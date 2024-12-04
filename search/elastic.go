348f8bb2d7a98b4f078000c7b7a738122e38606b
Luiggy102 <79861715+Luiggy102@users.noreply.github.com>
2024-12-04 12:45:10 -0500

    added concrete implementacion for elactic search: indexFeed

--- search/elastic.go
+++ search/elastic.go
@@ -1,6 +1,13 @@
 package search
 
-import "github.com/elastic/go-elasticsearch/v7"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+
+	"github.com/Luiggy102/go-cqrs-eda/models"
+	"github.com/elastic/go-elasticsearch/v7"
+)
 
 type ElacticSearchRepository struct {
 	client *elasticsearch.Client
@@ -17,7 +24,28 @@ func NewElastic(url string) (*ElacticSearchRepository, error) {
 	return &ElacticSearchRepository{client: client}, nil
 }
 
-// methost for the searchRepository interface
-// Close()
-// IndexFeed(ctx context.Context, feed models.Feed) error
-// SearchFeed(ctx context.Context, query string) ([]models.Feed, error)
+// methods for the searchRepository interface
+// func (r *ElacticSearchRepository) Close(){}
+func (r *ElacticSearchRepository) IndexFeed(ctx context.Context, feed models.Feed) error {
+	// create a json bytes for the feed
+	body, _ := json.Marshal(feed)
+
+	// index the feed to the client
+	_, err := r.client.Index(
+
+		"feeds",               // index name
+		bytes.NewReader(body), // reader with the data
+
+		// options
+		r.client.Index.WithDocumentID(feed.ID), // add an id to the document
+		r.client.Index.WithContext(ctx),        // contex for debug
+		r.client.Index.WithRefresh("wait_for"), // refresh the indices
+	)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// func (r *ElacticSearchRepository) SearchFeed(ctx context.Context, query string) ([]models.Feed, error){}
